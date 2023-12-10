21a58ac058808ea96592567e95676021a1b58873
jjmoey <jjmoey@gmail.com>
2023-12-10 23:16:43 +1100

    AWTR-2 - use sensor wrapper class

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,36 @@
 #include <Arduino.h>
 
 #include <DHT.h>
+#include "Constants.hpp"
+#include "service/MoistureSensor.hpp"
+
+using namespace AutoWater; 
 
 #define FLAG_PIN 2
-#define MOISTURE_SENSOR_PIN 4
+#define MOISTURE_SENSOR_PIN 35
 #define DHT_PIN 18
 #define DHT_TYPE DHT22   // DHT 22  (AM2302)
+
+MoistureSensor moistureSensor(MOISTURE_SENSOR_PIN); 
 DHT dht(DHT_PIN, DHT_TYPE); //// Initialize DHT sensor for normal 16mhz Arduino
 
 int moisture_sensor_threshold = 65; 
 
-long moisture_offset = 1071; 
-long moisture_max = 4095; 
-long moisture_rmin = 100; 
-long moisture_rmax = 0; 
-
 void setup() {
   Serial.begin(9600); 
   dht.begin(); 
+  moistureSensor.init(
+      Constants::DEFAULT_MOISTURE_MIN, 
+      Constants::DEFAULT_MOISTURE_MAX, 
+      Constants::DEFAULT_MOISTURE_OUTPUT_MIN, 
+      Constants::DEFAULT_MOISTURE_OUTPUT_MAX
+  ); 
 }
 
 void loop() {
   int raw_moisture = analogRead(MOISTURE_SENSOR_PIN);
-  float soil_pct_moisture = map(raw_moisture, moisture_offset, moisture_max, moisture_rmin, moisture_rmax);
+  moistureSensor.setRawValue(raw_moisture); 
+  float soil_pct_moisture = moistureSensor.readValue(); 
 
   float hum = dht.readHumidity();
   float temp = dht.readTemperature();
