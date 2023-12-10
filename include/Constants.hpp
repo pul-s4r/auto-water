fd99fc900e6ba2b35049360a4c2bb83891ea0caa
jjmoey <jjmoey@gmail.com>
2023-12-10 23:15:59 +1100

    AWTR-2 - add default sensor mappings

---
+++ include/Constants.hpp
@@ -0,0 +1,11 @@
+#ifndef _CONSTANTS_HPP
+#define _CONSTANTS_HPP
+
+namespace Constants {
+    extern const long DEFAULT_MOISTURE_MIN; 
+    extern const long DEFAULT_MOISTURE_MAX; 
+    extern const long DEFAULT_MOISTURE_OUTPUT_MIN; 
+    extern const long DEFAULT_MOISTURE_OUTPUT_MAX; 
+}
+
+#endif
---
+++ src/Constants.cpp
@@ -0,0 +1,8 @@
+#include "Constants.hpp"
+
+namespace Constants {
+    extern const long DEFAULT_MOISTURE_MIN = 1071; 
+    extern const long DEFAULT_MOISTURE_MAX = 4095; 
+    extern const long DEFAULT_MOISTURE_OUTPUT_MIN = 100; 
+    extern const long DEFAULT_MOISTURE_OUTPUT_MAX = 0; 
+}
